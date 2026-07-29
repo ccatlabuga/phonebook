f70f746e7086ed705fc0c7423c8558fe8898e16f
ccatlabuga <ccatlabuga@griddynamics.com>
2026-07-29 14:29:13 +0300

    add tests for addPhone

--- src/test/java/com/phonebook/tests/PhoneBookTest.java
+++ src/test/java/com/phonebook/tests/PhoneBookTest.java
@@ -1,36 +1,72 @@
 package com.phonebook.tests;
 
+import com.phonebook.config.ApplicationConfig;
+import com.phonebook.core.exception.UserNotFoundException;
 import com.phonebook.core.service.PhoneBook;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.List;
 
-import static java.util.Arrays.asList;
-import static org.junit.Assert.assertEquals;
-
+@ExtendWith(SpringExtension.class)
+@SpringJUnitConfig(classes = { ApplicationConfig.class })
 public class PhoneBookTest {
     @Autowired
     private PhoneBook phoneBook;
 
     @Test
     public void addSinglePhoneTest() {
+        String NAME = "Alex";
+        String PHONE = "test";
 
+        this.phoneBook.addPhone(NAME, PHONE);
+        Assertions.assertTrue(this.phoneBook.findAllPhonesByName(NAME).contains(PHONE));
+    }
+
+    @Test
+    public void addSinglePhoneWithNonexistentNameThrowsTest() {
+        String NAME = "Notexist";
+        String PHONE = "test";
+
+        Assertions.assertThrows(UserNotFoundException.class, () -> this.phoneBook.addPhone(NAME, PHONE));
     }
 
     @Test
     public void addListOfPhonesTest() {
+        String NAME = "Alex";
+        List<String> PHONES = List.of("test1", "test2");
 
+        this.phoneBook.addPhone(NAME, PHONES);
+        Assertions.assertTrue(this.phoneBook.findAllPhonesByName(NAME).containsAll(PHONES));
     }
 
     @Test
-    public void addPhonesWithArgsTest() {
+    public void addListOfPhonesWithNonexistentNameThrowsTest() {
+        String NAME = "Notexist";
+        List<String> PHONES = List.of("test1", "test2");
 
+        Assertions.assertThrows(UserNotFoundException.class, () -> this.phoneBook.addPhone(NAME, PHONES));
     }
 
     @Test
-    public void removeExistingPhoneTest() {
+    public void addListOfPhonesAsArgsTest() {
+        List<String> ARGS = List.of("Alex", "test3,test4");
 
+        this.phoneBook.addPhone(ARGS);
+        Assertions.assertTrue(this.phoneBook.findAllPhonesByName(ARGS.getFirst())
+                .containsAll(Arrays.stream(ARGS.getLast().split(",")).toList()));
+    }
+
+    @Test
+    public void addListOfPhonesWithNonexistentNameAsArgsThrowsTest() {
+        List<String> ARGS = List.of("Noexist", "test3,test4");
+
+        Assertions.assertThrows(UserNotFoundException.class, () -> this.phoneBook.addPhone(ARGS));
     }
 }
